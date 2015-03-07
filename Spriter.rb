020c91d84fda06d49d9028e81b0043004b6a96cf
Mau Magnaguagno <mauricio_c_m@hotmail.com>
2015-03-07 05:10:08 -0300

    Update Spriter.rb
    Fall off x outside inner loop (speed)
    Correct xy mirroring (fix)

--- Spriter.rb
+++ Spriter.rb
@@ -15,6 +15,9 @@
 # - Added seed
 # Feb 2015
 # - Optimized
+# Mar 2015
+# - Fall off x outside inner loop
+# - Correct xy mirroring
 #-----------------------------------------------
 # TODOs
 # - Optional clean step
@@ -96,11 +99,10 @@ class Spriter
     limit_y = mirror_y ? range_y : @height
     # Draw
     index_y = 0
+    falloff_x = Array.new(limit_x) {|x| send(falloff, range_x, x)}
     limit_y.times {|y|
       falloff_y = probmax * send(falloff, range_y, y)
-      limit_x.times {|x|
-        @grid[x + index_y] = 1 if rand(101) <= probmin + falloff_y * send(falloff, range_x, x)
-      }
+      limit_x.times {|x| @grid[x + index_y] = 1 if rand(101) <= probmin + falloff_y * falloff_x[x]}
       index_y += @width
     }
     # Clean
@@ -127,47 +129,51 @@ class Spriter
     # Mirror
 =begin
     index_y = 0
-    if mirror_x
-      if mirror_y
-        index_mirror = (@width * @height).pred
-        limit_y.times {
-          limit_x.times {|x| @grid[index_mirror - x] = @grid[x + index_y]}
-          index_mirror -= @width
-          index_y += @width
-        }
-      else
-        index_mirror = @width.pred
-        limit_y.times {
-          limit_x.times {|x| @grid[index_mirror - x] = @grid[x + index_y]}
-          index_mirror += @width
-          index_y += @width
-        }
-      end
-    elsif mirror_y
-      index_mirror = @height.pred * @width
-      limit_y.times {
-        limit_x.times {|x| @grid[x + index_mirror] = @grid[x + index_y]}
-        index_mirror -= @width
-        index_y += @width
-      }
-    end
-=end
-    index_y = color = 0
     limit_y.times {|y|
       limit_x.times {|x|
-        color = @grid[x + index_y]
         if mirror_x
+          color = @grid[x + index_y]
           @grid[@width.pred - x + index_y] = color
-        end
-        if mirror_y
-          @grid[x + (@height.pred - y) * @width] = color
-          if mirror_x
+          if mirror_y
+            @grid[x + (@height.pred - y) * @width] = color
             @grid[@width.pred - x + (@height.pred - y) * @width] = color
           end
+        elsif mirror_y
+          @grid[x + (@height.pred - y) * @width] = @grid[x + index_y]
         end
       }
       index_y += @width
     }
+=end
+    index_y = 0
+    if mirror_y
+      if mirror_x
+        mirror_xy = (@width * @height).pred
+        mirror_x = @width.pred
+        mirror_y = mirror_xy - mirror_x
+        limit_y.times {
+          limit_x.times {|x| @grid[mirror_x - x] = @grid[mirror_y + x] = @grid[mirror_xy - x] = @grid[x + index_y]}
+          index_y += @width
+          mirror_xy -= @width
+          mirror_x += @width
+          mirror_y -= @width
+        }
+      else
+        mirror_y = @height.pred * @width
+        limit_y.times {
+          limit_x.times {|x| @grid[mirror_y + x] = @grid[x + index_y]}
+          mirror_y -= @width
+          index_y += @width
+        }
+      end
+    elsif mirror_x
+      mirror_x = @width.pred
+      limit_y.times {
+        limit_x.times {|x| @grid[mirror_x - x] = @grid[x + index_y]}
+        mirror_x += @width
+        index_y += @width
+      }
+    end
   end
 
   #-----------------------------------------------
@@ -220,7 +226,7 @@ if $0 == __FILE__
   begin
     t = Time.now.to_f
     div = '-' * 32
-    ext = 'png'
+    ext = 'bmp'
     100.times {|seed|
       spt = Spriter.new(32, 32, seed)
       spt.generate(87, 13)
