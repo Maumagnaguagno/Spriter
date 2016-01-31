5912376857e31b1747d14f220779cb6d7ded2abb
Mau Magnaguagno <mauricio.magnaguagno@acad.pucrs.br>
2016-01-31 17:04:21 -0200

    Massive refactoring
    - Fixed (w,h) to (width, height)
    - Fixed get_pixel bug
    - Use @width, @height instead of width, height
    - 'BM' to short integer
    - Removed intermediary variables when possible
    - Removed conditionals from loops (alpha test)
    - Use integer instead of hex char

--- Image.rb
+++ Image.rb
@@ -1,12 +1,12 @@
 require 'zlib'
 
-#-----------------------------------------------
-# Image
-#-----------------------------------------------
-
 class Image
 
   attr_reader :width, :height
+
+  #-----------------------------------------------
+  # Initialize
+  #-----------------------------------------------
 
   def initialize(width, height)
     @width = width
@@ -19,7 +19,7 @@ class Image
   #-----------------------------------------------
 
   def set_pixel(x, y, r, g, b, a)
-    @pixels[x + y * width << 2, 4] = [b,g,r,a].pack('C4')
+    @pixels[x + y * @width << 2, 4] = [b,g,r,a].pack('C4')
     self
   end
 
@@ -28,8 +28,11 @@ class Image
   #-----------------------------------------------
 
   def get_pixel(x, y)
-    index = x + y * width << 2
-    @pixels[index, 3].reverse.unpack('C3') << @pixels[index + 3].unpack('C')
+    pixel = @pixels[x + y * @width << 2, 4].unpack('C4')
+    b = pixel[0]
+    pixel[0] = pixel[2]
+    pixel[2] = b
+    pixel
   end
 
   #-----------------------------------------------
@@ -50,39 +53,38 @@ class Image
     self
   end
 
-  BMP_HEADER = 'a2ls2l4s2l6'
+  BMP_HEADER = 'sls2l4s2l6'
   RGB = 2
   RGBA = 6
 
-  #--------------------------------------------------------------
+  #-----------------------------------------------
   # Header BMP
-  #--------------------------------------------------------------
+  #-----------------------------------------------
 
-  def self.header_bmp(w, h)
+  def self.header_bmp(width, height)
     # BMP header
-    ['BM', (((24 * w + 31) >> 5) * h << 2) + 54, 0, 0, 54,
+    [19778, (((24 * width + 31) >> 5) * height << 2) + 54, 0, 0, 54,
     # DIB header
-    40, w, h, 1, 24, 0, 0, 0, 0, 0, 0].pack(BMP_HEADER)
+    40, width, height, 1, 24, 0, 0, 0, 0, 0, 0].pack(BMP_HEADER)
   end
 
-  #--------------------------------------------------------------
+  #-----------------------------------------------
   # Load BMP
-  #--------------------------------------------------------------
+  #-----------------------------------------------
 
   def self.load_bmp2(filename)
     image = nil
     open(filename,'rb') {|file|
-      header = file.read(54)
-      header_unpack = header.unpack(BMP_HEADER)
-      w = header_unpack[6]
-      h = header_unpack[7]
-      image = new(w, h)
-      padding = w & 3
-      w3 = w << 3
-      index_image = (w - 1) * h << 2
-      h.times {
-        w.times {
-          image.write(index_image, 0, file.read(3) << "\xFF", 4)
+      header_unpack = file.read(54).unpack(BMP_HEADER)
+      width = header_unpack[6]
+      height = header_unpack[7]
+      image = new(width, height)
+      padding = width & 3
+      w3 = width << 3
+      index_image = width.pred * height << 2
+      heigt.times {
+        width.times {
+          image.write(index_image, 0, file.read(3) << 255, 4)
           index_image += 4
         }
         index_image -= w3
@@ -92,9 +94,9 @@ class Image
     image
   end
 
-  #--------------------------------------------------------------
+  #-----------------------------------------------
   # Save BMP
-  #--------------------------------------------------------------
+  #-----------------------------------------------
 
   def save_bmp(filename)
     open(filename,'wb') {|file|
@@ -118,14 +120,13 @@ class Image
     }
   end
 
-  #--------------------------------------------------------------
+  #-----------------------------------------------
   # Load PNG
-  #--------------------------------------------------------------
+  #-----------------------------------------------
 
   def self.load_png(filename)
     image = nil
-    w = h = 0
-    color_type = 0
+    width = height = color_type = 0
     open(filename,'rb') {|file|
       # Signature
       file.pos += 8
@@ -137,28 +138,27 @@ class Image
         case type
         when 'IHDR'
           data = data.unpack('N2C5')
-          w = data.first
-          h = data[1]
+          width = data.first
+          height = data[1]
           color_type = data[3]
-          image = new(w, h)
+          image = new(width, height)
         when 'IDAT'
           data = Zlib::Inflate.inflate(data)
-          index = 0
-          index_image = 0
+          index = index_image = 0
           if color_type == RGB
-            h.times {
+            height.times {
               index += 1
-              w.times {
-                image.write(index_image, 0, data[index,3].reverse << "\xFF", 4)
+              width.times {
+                image.write(index_image, 0, data[index,3].reverse! << 255, 4)
                 index += 3
                 index_image += 4
               }
             }
           else
-            h.times {
+            height.times {
               index += 1
-              w.times {
-                image.write(index_image, 0, data[index,3].reverse << data[index+3], 4)
+              width.times {
+                image.write(index_image, 0, data[index,3].reverse! << data[index+3], 4)
                 index += 4
                 index_image += 4
               }
@@ -172,49 +172,55 @@ class Image
     image
   end
 
-  #--------------------------------------------------------------
+  #-----------------------------------------------
   # Save PNG
-  #--------------------------------------------------------------
+  #-----------------------------------------------
 
   def save_png(filename, color_type = RGBA)
     size = width * height << 2
     data = ' ' * size
     read(0, 0, data, size)
     img_data = ''
-    index = 0
-    alpha = color_type == RGBA
-    height.times {
-      img_data << "\0"
-      width.times {
-        img_data << data[index, 3].reverse
-        img_data << data[index+3] if alpha
-        index += 4
+    if color_type == RGBA
+      index = 0
+      height.times {
+        img_data << 0
+        width.times {
+          img_data << data[index, 3].reverse! << data[index+3]
+          index += 4
+        }
       }
-    }
+    else
+      index = -4
+      height.times {
+        img_data << 0
+        width.times {img_data << data[index += 4, 3].reverse!}
+      }
+    end
     Image.save_png_data(filename, img_data, width, height, color_type)
   end
 
-  #--------------------------------------------------------------
+  #-----------------------------------------------
   # Save PNG data
-  #--------------------------------------------------------------
+  #-----------------------------------------------
 
-  def self.save_png_data(filename, data, w, h, color_type)
+  def self.save_png_data(filename, data, width, height, color_type)
     open(filename,'wb') {|file|
-      file << "\x89PNG\r\n\x1a\n"
-      file << chunk('IHDR', [w, h, 8, color_type, 0, 0, 0].pack('N2C5'))
-      file << chunk('IDAT', Zlib::Deflate.deflate(data, 9))
-      file << "\0\0\0\0IEND\xAEB`\x82"
+      file << "\x89PNG\r\n\x1a\n" <<
+              chunk('IHDR', [width, height, 8, color_type, 0, 0, 0].pack('N2C5')) <<
+              chunk('IDAT', Zlib::Deflate.deflate(data, 9)) <<
+              "\0\0\0\0IEND\xAEB`\x82"
     }
   end
 
-  #--------------------------------------------------------------
+  #-----------------------------------------------
   # Chunk
-  #--------------------------------------------------------------
+  #-----------------------------------------------
 
   def self.chunk(type, data)
     [data.size, type, data, Zlib.crc32(type << data)].pack('NA4A*N')
   end
-  
+
   #-----------------------------------------------
   # Save SVG
   #-----------------------------------------------
