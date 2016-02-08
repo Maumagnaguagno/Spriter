da1d4c1361e915654ea0732646ba59222e06edb9
Mau Magnaguagno <mauricio.magnaguagno@acad.pucrs.br>
2016-02-07 23:38:45 -0200

    Test dup

--- tests/pixels.rb
+++ tests/pixels.rb
@@ -3,16 +3,26 @@ require './Image'
 
 class Pixels < Test::Unit::TestCase
 
-  def test_image_dimensions
+  def test_dimensions
     img = Image.new(2,3)
     assert_equal(2, img.width)
     assert_equal(3, img.height)
   end
 
-  def test_image_get_set_pixels
+  def test_get_set_pixels
     img = Image.new(1,1)
     assert_equal([0,0,0,0], img.get_pixel(0,0))
-    assert_equal(img, img.set_pixel(0,0, 100, 150, 200, 255))
+    assert_equal(img, img.set_pixel(0,0, 100,150,200,255))
     assert_equal([100,150,200,255], img.get_pixel(0,0))
+  end
+
+  def test_dup
+    img = Image.new(1,1)
+    dup = img.set_pixel(0,0, 100,150,200,255).dup
+    assert_equal([100,150,200,255], img.get_pixel(0,0))
+    assert_equal([100,150,200,255], dup.get_pixel(0,0))
+    img.set_pixel(0,0, 1,1,1,1)
+    assert_equal([1,1,1,1], img.get_pixel(0,0))
+    assert_equal([100,150,200,255], dup.get_pixel(0,0))
   end
 end
