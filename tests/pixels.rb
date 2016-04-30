122e4991963f1f835c5d35c9c88195811c346afa
Mau Magnaguagno <maumagnaguagno@gmail.com>
2016-04-30 07:29:09 -0300

    Match pixels

--- tests/pixels.rb
+++ tests/pixels.rb
@@ -14,16 +14,22 @@ class Pixels < Test::Unit::TestCase
     dup = img.set_pixel(0,0, 100,150,200,255).dup
     assert_equal([100,150,200,255], img.get_pixel(0,0))
     assert_equal([100,150,200,255], dup.get_pixel(0,0))
+    assert_equal(true, img.match_pixel?(0,0, 100,150,200,255))
+    assert_equal(true, dup.match_pixel?(0,0, 100,150,200,255))
     img.set_pixel(0,0, 1,1,1,1)
     assert_equal([1,1,1,1], img.get_pixel(0,0))
     assert_equal([100,150,200,255], dup.get_pixel(0,0))
+    assert_equal(true, img.match_pixel?(0,0, 1,1,1,1))
+    assert_equal(true, dup.match_pixel?(0,0, 100,150,200,255))
   end
 
-  def test_get_set_pixels
+  def test_get_set_match_pixels
     img = Image.new(1,1)
     assert_equal([0,0,0,0], img.get_pixel(0,0))
+    assert_equal(true, img.match_pixel?(0,0, 0,0,0,0))
     assert_equal(img, img.set_pixel(0,0, 100,150,200,255))
     assert_equal([100,150,200,255], img.get_pixel(0,0))
+    assert_equal(true, img.match_pixel?(0,0, 100,150,200,255))
   end
 
   def test_read_write
